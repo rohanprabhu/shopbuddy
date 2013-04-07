9316c9a09801805d70b06e628af170c7b0eb3010
Rohan Prabhu <rprabhu@rprabhu.desktop.amazon.com>
2013-04-07 16:07:04 +0530

    Chat is now working

--- scripts/main.js
+++ scripts/main.js
@@ -1,112 +1,149 @@
-var userId;
+var getURLParameter = function(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+};
+
+var userId = getURLParameter("userId");
+
+var ShopBuddyClient = function() {
+    this.pushMessage = function(namespace, message) {
+        var message = (typeof message != "string")?JSON.stringify(message):message;
+        $.ajax({
+            url: "http://ayushch.desktop.amazon.com:8000",
+            headers: {
+                "X-Amz-Target": "com.amazon.shopbuddy.ShopBuddyService.PushMessage",
+                "Content-Encoding": "amz-1.0",
+                "X-Requested-With": "XMLHttpRequest"
+            },
+            contentType: "application/json",
+            type: "POST",
+            data: JSON.stringify({
+                "message": {
+                    "body": message
+                },
+                "namespace": namespace
+            })
+        });
+    };
+
+    this.getMessages = function(namespace, id, callback) {
+        var message = JSON.stringify(message);
+        var dt = JSON.stringify({"namespace": namespace, "id": id});
+
+        $.ajax({
+            url: "http://ayushch.desktop.amazon.com:8000",
+            headers: {
+                "X-Amz-Target": "com.amazon.shopbuddy.ShopBuddyService.GetMessages",
+                "Content-Encoding": "amz-1.0",
+                "X-Requested-With": "XMLHttpRequest"
+            },
+            contentType: "application/json",
+            type: "POST",
+            data: JSON.stringify({
+                "namespace": namespace,
+                "id": id
+            })
+        }).done(callback);
+    };
+};
+
+var shopBuddyClient = new ShopBuddyClient();
 
 var plainPushMessage = function(message) {
-	document.getElementById("chatBox").innerHTML += '<div class="message">' +
-	    '<div class="userId" style="color: ' + colorHash(message.userId) + '">' +
-		    message.userId +
-		  '</div>' +
-		  '<div class="userMessage">' +
-		    message.payload +
-		  '</div>' + 
-		  '<div class="clears"></div>' +
-		'</div>';
+    document.getElementById("chatBox").innerHTML += '<div class="message">' +
+        '<div class="userId" style="color: ' + colorHash(message.userId) + '">' +
+            message.userId +
+          '</div>' +
+          '<div class="userMessage">' +
+            message.payload +
+          '</div>' + 
+          '<div class="clears"></div>' +
+        '</div>';
 
-	scrollToTop();
+    scrollToTop();
 };
 
 var colorHash = function(string) {
-	var index = 0;
-	var colors = [
+    var index = 0;
+    var colors = [
         "#FF0000",
         "#00FF00",
         "#0032FF"
     ];
 
     for(var i=0; i<string.length; i++) {
-    	index += string.charCodeAt(i);
+        index += string.charCodeAt(i);
     }
 
     return colors[index % colors.length];
 };
 
 var scrollToTop = function() {
-	document.getElementById("chatBox").scrollTop = document.getElementById("chatBox").scrollHeight;
-};
-
-var adjustChatBox = function() {
-  var height = document.getElementById('chatBox').offsetHeight - 200;
-  document.getElementById('rest').style.height = height + 'px';
+    document.getElementById("chatBox").scrollTop = document.getElementById("chatBox").scrollHeight;
 };
 
 var pushMessage = function(message) {
-	switch(message.pushType) {
-		case "PlainText": {
-			plainPushMessage(message);
-		} break;
-	};
+    switch(message.pushType) {
+        case "PlainText": {
+            plainPushMessage(message);
+        } break;
+    };
 
-	scrollToTop();
+    scrollToTop();
 };
 
 var Poller = function(namespace, callback) {
-	var pollUrl = "",
-	    latestId = null;
+    var latestId = null;
 
-	var polling = function() {
-    	latestId = json.lastId;
+    var semiCallback = function(data) {
+        var data = JSON.parse(data);
+        latestId = data.lastId || "-1";
+        callback(data.messages);
+    };
 
-    	$.ajax({
-    	    url: pollUrl,
-    	    data: '{"namespace": ' + namespace + ', "lastId": ' + latestId + '}',
-    	    dataType: 'application/json'
-    	}).done(function(data) {
-    	   	latestId = data.lastId;
-    	   	callback(data.messages);
-    	});
-	};
+    var polling = function() {
+        shopBuddyClient.getMessages(namespace, latestId, semiCallback);
+        setTimeout(polling, 120);
+    };
 
-    $.ajax({
-    	url: pollUrl,
-    	data: '{"namespace": ' + namespace + ', "lastId": -1}',
-    	dataType: 'application/json'
-    }).done(function(data) {
-    	latestId = data.lastId;
-    	setInterval(polling, 120);
+    shopBuddyClient.getMessages(namespace, "-1", function(data) {
+        var data = JSON.parse(data);
+        latestId = data.lastId || "-1";
+        polling();
     });
 };
 
 var postMessage = function(namespace, message) {
-	var postUrl = "";
-
-    $.ajax({
-    	url: postUrl,
-    	data: JSON.stringify({
-    		"namespace": namespace,
-    		"message": message
-    	})
-    });
+    shopBuddyClient.pushMessage(namespace, message);
 };
 
 $(document).ready(function() {
-	var simpleMessagePoller = new Poller("simpleMessages", function(messages) {
-		for(var i=0; i<messages.length; i++) {
-			plainPushMessage(messages[i]);
-		}
-	});
+    var simpleMessagePoller = new Poller("simpleMessages", function(messages) {
+        if(typeof messages == "undefined" || messages == null) {
+            return;
+        }
 
-	$("#chatLineInp").keypress(function(event) {
-		if(event.which == 13) {
-			event.preventDefault();
-		}
+        for(var i=0; i<messages.length; i++) {
+            console.log(messages[i]);
+            var messageObject = JSON.parse(messages[i].body);
+            plainPushMessage(messageObject);
+        }
+    });
 
-		postMessage("simpleMessages",
-		    JSON.stringify({
-			    userId: userId,
-			    message: $("#chatLineInp").value()
-		    })
-		);
+    $("#chatLineInp").keypress(function(event) {
+        if(event.which == 13) {
+            event.preventDefault();
 
-		$("#chatLineInp").value("");
-		$("#chatLineInp").focus();
-	});
+            postMessage("simpleMessages",
+                {
+                    userId: userId,
+                    payload: $("#chatLineInp").val()
+                }
+            );
+
+            $("#chatLineInp").val("");
+            $("#chatLineInp").focus();
+        }
+    });
 });
--- tamper/tamper.js
+++ tamper/tamper.js
@@ -11,6 +11,48 @@ var shown = false;
 var made = false;
 var ifrm;
 
+var ShopBuddyClient = function() {
+    this.pushMessage = function(namespace, message) {
+        var message = (typeof message != "string")?JSON.stringify(message):message;
+        $.ajax({
+            url: "http://ayushch.desktop.amazon.com:8000",
+            headers: {
+                "X-Amz-Target": "com.amazon.shopbuddy.ShopBuddyService.PushMessage",
+                "Content-Encoding": "amz-1.0",
+                "X-Requested-With": "XMLHttpRequest"
+            },
+            contentType: "application/json",
+            type: "POST",
+            data: JSON.stringify({
+                "message": {
+                    "body": message
+                },
+                "namespace": namespace
+            })
+        });
+    };
+
+    this.getMessages = function(namespace, id, callback) {
+        var message = JSON.stringify(message);
+        var dt = JSON.stringify({"namespace": namespace, "id": id});
+
+        $.ajax({
+            url: "http://ayushch.desktop.amazon.com:8000",
+            headers: {
+                "X-Amz-Target": "com.amazon.shopbuddy.ShopBuddyService.GetMessages",
+                "Content-Encoding": "amz-1.0",
+                "X-Requested-With": "XMLHttpRequest"
+            },
+            contentType: "application/json",
+            type: "POST",
+            data: JSON.stringify({
+                "namespace": namespace,
+                "id": id
+            })
+        }).done(callback);
+    };
+};
+
 try {
     document.getElementById("buyBoxContent").innerHTML =
         '<h3 style="text-align: center; padding: 5pt; margin: 3pt; font-size: 15pt; background: #E8E8E8; border: 1px solid #000000">Ask my buddies</h3>' + document.getElementById("buyBoxContent").innerHTML;
